ffb9841ca5f110e2a8755c6b3df4ee690f67912a
Дмитрий Захаров <zds.vip1221@gmail.com>
2025-01-23 12:04:35 +0300

    Initial commit

---
+++ src/main/java/pro/sky/listspring/ListSpringApplication.java
@@ -0,0 +1,14 @@
+package pro.sky.listspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListSpringApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ListSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/listspring/ListSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.listspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
